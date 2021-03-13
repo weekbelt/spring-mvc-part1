7ab935c320c166acc71baaa06a40c4c34fd8e071
weekbelt <vfrvfr4207@gmail.com>
2021-03-13 22:50:20 +0900

    feat: HTTP 요청 데이터 - API 메시지 바디 - JSON & HttpServletResponse 사용법

---
+++ src/main/java/hello/servlet/basic/request/RequestBodyJsonServlet.java
@@ -0,0 +1,33 @@
+package hello.servlet.basic.request;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import hello.servlet.basic.HelloData;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import javax.servlet.ServletInputStream;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.util.StreamUtils;
+
+@WebServlet(name = "requestBodyJsonServlet", urlPatterns = "/request-body-json")
+public class RequestBodyJsonServlet extends HttpServlet {
+
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        final ServletInputStream inputStream = request.getInputStream();
+        final String messageBody = StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);
+
+        System.out.println("messageBody = " + messageBody);
+
+        final HelloData helloData = objectMapper.readValue(messageBody, HelloData.class);
+
+        System.out.println("helloData.username = " + helloData.getUsername());
+        System.out.println("helloData.age = " + helloData.getAge());
+
+        response.getWriter().write("ok");
+    }
+}
