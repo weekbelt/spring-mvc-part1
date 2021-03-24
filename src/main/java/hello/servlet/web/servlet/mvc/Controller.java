58317c45bb6ab48ad15df674ae85d2c3bef5e977
weekbelt <vfrvfr4207@gmail.com>
2021-03-24 10:43:34 +0900

    feat: Old Spring MVC

---
+++ src/main/java/hello/servlet/web/servlet/mvc/Controller.java
@@ -0,0 +1,11 @@
+package hello.servlet.web.servlet.mvc;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.web.servlet.ModelAndView;
+
+public interface Controller {
+
+    ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception;
+
+}
